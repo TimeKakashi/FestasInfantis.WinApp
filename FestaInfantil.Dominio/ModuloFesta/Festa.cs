ab3a397356e81421c37d8985b8824cb451850a10
Felipe Maines <felipemaines123@gmail.com>
2023-06-09 14:44:51 -0300

    arrumado o dominio

---
+++ FestaInfantil.Dominio/ModuloFesta/Festa.cs
@@ -0,0 +1,27 @@
+﻿using FestaInfantil.Dominio.ModuloCliente;
+using FestaInfantil.Dominio.ModuloCompartilhado;
+using FestaInfantil.Dominio.ModuloTema;
+
+namespace FestaInfantil.Dominio.ModuloFesta
+{
+    public class Festa : EntidadeBase
+    {
+        public Tema tema { get; set; }
+        public Cliente cliente { get; set; }
+        public string enderecoFesta { get; set; }
+        public DateTime data { get; set; }
+        public TimeSpan horaInicio { get; set; }
+        public TimeSpan horaFinal { get; set; }
+        public decimal valorEntrada { get; set; }
+
+
+        public Festa(string enderecoFesta, Cliente cliente, DateTime data, TimeSpan horaInicio, TimeSpan horaFinal)
+        {
+            this.enderecoFesta = enderecoFesta;
+            this.cliente = cliente;
+            this.data = data;
+            this.horaInicio = horaInicio;
+            this.horaFinal = horaFinal;
+        }
+    }
+}
