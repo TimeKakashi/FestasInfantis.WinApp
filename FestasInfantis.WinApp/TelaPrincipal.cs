9286a20b0c605985e75a370366dc0e702e6fef85
jggomes14 <jggomes141097@gmail.com>
2023-06-09 16:51:45 -0300

    Revert "Funcionamento da tela de contato"
    This reverts commit 0d2c70c3e0e93a84aee8d6dd694cd78638c78969.

--- FestasInfantis.WinApp/TelaPrincipal.cs
+++ FestasInfantis.WinApp/TelaPrincipal.cs
@@ -1,52 +1,15 @@
-using FestaInfantil.Dominio.ModuloCliente;
-using FestasInfantis.WinApp.Compartilhado;
-using FestasInfantis.WinApp.ModuloCliente;
-
 namespace FestasInfantis.WinApp
 {
     public partial class TelaPrincipal : Form
     {
-        private ControladorBase controlador;
-        private RepositorioCliente repositorioCliente = new RepositorioCliente();
-        private static TelaPrincipal telaPrincipal;
         public TelaPrincipal()
         {
             InitializeComponent();
         }
-        private void ConfigurarTelaPrincipal(ControladorBase controladorBase)
-        {
-            labelTipoCadastro.Text = controlador.ObterTipoCadastro();
-            ConfigurarToolTips(controlador);
-            ConfigurarListagem(controlador);
 
-        }
-        public void AtualizarBarraDeStatus(string mensagem)
-        {
-            labelRodape.Text = mensagem;
-        }
-        private void ConfigurarListagem(ControladorBase controladorBase)
-        {
-            UserControl listagem = controladorBase.ObterListagem();
-            listagem.Dock = DockStyle.Fill;
-            panelRegistros.Controls.Clear();
-            panelRegistros.Controls.Add(listagem);
-
-        }
-        private void ConfigurarToolTips(ControladorBase controlador)
-        {
-            btnInserir.Text = controlador.ToolTipInserir;
-            btnEditar.Text = controlador.ToolTipEditar;
-            btnExcluir.Text = controlador.ToolTipExcluir;
-        }
-
-
-        private void clientesMenuItem_Click(object sender, EventArgs e)
+        private void toolStripLabel1_Click(object sender, EventArgs e)
         {
 
-            controlador = new ControladorCliente(repositorioCliente);
-            ConfigurarTelaPrincipal(controlador);
         }
-
-
     }
 }
